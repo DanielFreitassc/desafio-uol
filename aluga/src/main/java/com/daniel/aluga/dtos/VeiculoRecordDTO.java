5b3fd43b91fadde0d31bac9d6b6c36316074b60c
DanielFreitassc <danielfreitassc@gmail.com>
2024-07-23 00:01:03 -0300

    Feat: implementado regras de negocio

--- aluga/src/main/java/com/daniel/aluga/dtos/VeiculoRecordDTO.java
+++
@@ -1,8 +0,0 @@
-package com.daniel.aluga.dtos;
-
-import java.math.BigDecimal;
-import java.util.Date;
-
-public record VeiculoRecordDTO(String tipo , String cidade, BigDecimal valor, Date alugadoEm, Date devolvidoEm) {
-    
-}
